1fc2e47ce63bdd3a0f3934a18d493403310482a9
Rakshit <sarakshi@lowes.com>
2020-11-04 00:30:02 +0530

    Saswata: Organization onboard first checkin

--- code_base/web-ui/src/App.js
+++ code_base/web-ui/src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import OrganisationHeader from "./components/organisation_onborading/organisationHeader/OrganisationHeader";
-import OrganisationBase from "./components/organisation_onborading/organisationBase/OrganisationBase";
-import OrganisationFooter from "./components/organisation_onborading/organisationFooter/OrganisationFooter";
+// import OrganisationHeader from "./components/organisation_onborading/organisationHeader/OrganisationHeader";
+// import OrganisationBase from "./components/organisation_onborading/organisationBase/OrganisationBase";
+// import OrganisationFooter from "./components/organisation_onborading/organisationFooter/OrganisationFooter";
 
-function App() {
-  return (
-    <div className="App">
-       <OrganisationHeader/>
-       <OrganisationBase/>
-       <OrganisationFooter/>
-    </div>
-  );
+import { BrowserRouter } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout'
+import SetupBuilder from './container/SetupBuilder/SetupBuilder'
+
+class App extends Component {
+  render() {
+    return (
+      // <div className="App">
+      //    <OrganisationHeader/>
+      //    <OrganisationBase/>
+      //    <OrganisationFooter/>
+      // </div>
+      <BrowserRouter>
+        <Layout />
+        <SetupBuilder />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
