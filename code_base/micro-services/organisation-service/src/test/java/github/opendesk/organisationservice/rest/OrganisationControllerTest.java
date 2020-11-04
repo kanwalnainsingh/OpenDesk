0a7b8a1943122cd446056cc305bf48f08a640a2f
Minal2425 <66628971+Minal2425@users.noreply.github.com>
2020-11-04 23:20:52 +0530

    #42: Adding maven wrapper and docker setup (#45)
    * Adding a test file to new_branch
    
    * #12:creating organisation-service
    
    * adding files
    
    * #12:creating organisation-service
    
    * #12:adding README and test cases for OrganisationService
    
    * #12: resolving conflict with main
    
    * #12:removing redundant spaces
    
    * #42: Adding JSONPath in test cases
    
    * #42: Adding Dockerfile
    
    Co-authored-by: Minal <minalgarg.93@gmail.com>

--- code_base/micro-services/organisation-service/src/test/java/github/opendesk/organisationservice/rest/OrganisationControllerTest.java
+++ code_base/micro-services/organisation-service/src/test/java/github/opendesk/organisationservice/rest/OrganisationControllerTest.java
@@ -1,8 +1,10 @@
 package github.opendesk.organisationservice.rest;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import github.opendesk.organisationservice.base.BaseTest;
-import github.opendesk.organisationservice.model.Organisation;
 import github.opendesk.organisationservice.service.OrganisationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +16,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -26,35 +29,39 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class OrganisationControllerTest extends BaseTest {
     @Autowired
     private MockMvc mockMvc;
-
     @MockBean
     private OrganisationService organisationService;
-    private static final String expectedResponse = "{\"orgId\":\"orgId\",\"companyName\":\"companyName1\",\"city\":\"city\",\"headQuarters\":\"headQuarters\",\"sites\":[{\"siteId\":\"siteId\",\"name\":\"site1\",\"floors\":[{\"floorId\":\"floorId\",\"name\":\"name\",\"totalSeat\":\"totalSeat\",\"siteId\":\"siteId\"}],\"orgId\":\"orgId\"}]}";
-    private static final String expectedResponseList = "[{\"orgId\":\"orgId\",\"companyName\":\"companyName1\",\"city\":\"city\",\"headQuarters\":\"headQuarters\",\"sites\":[{\"siteId\":\"siteId\",\"name\":\"site1\",\"floors\":[{\"floorId\":\"floorId\",\"name\":\"name\",\"totalSeat\":\"totalSeat\",\"siteId\":\"siteId\"}],\"orgId\":\"orgId\"}]}]";
-    private static final String expectedSiteResponse = "{\"siteId\":\"siteId\",\"name\":\"site1\",\"floors\":[{\"floorId\":\"floorId\",\"name\":\"name\",\"totalSeat\":\"totalSeat\",\"siteId\":\"siteId\"}],\"orgId\":\"orgId\"}";
+    private DocumentContext organisationContext;
+    private DocumentContext organisationListContext;
+    private DocumentContext siteContext;
 
+
+    @BeforeEach
+    public void setup() throws IOException {
+        File organisationFile = new File("src/test/resources/organisationSample.json");
+        organisationContext = JsonPath.parse(organisationFile);
+        File organisationListFile = new File("src/test/resources/organisationListSample.json");
+        organisationListContext = JsonPath.parse(organisationListFile);
+        File siteFile = new File("src/test/resources/siteSample.json");
+        siteContext = JsonPath.parse(siteFile);
+    }
 
     @Test
     public void getOrganisationByIdTest() throws Exception {
         when(organisationService.getOrganisationById(anyString())).thenReturn(getOrganisation());
-        RequestBuilder request = MockMvcRequestBuilders.get("/organisation/{id}","orgId").accept(MediaType.APPLICATION_JSON);
+        RequestBuilder request = MockMvcRequestBuilders.get("/organisation/{id}", "orgId").accept(MediaType.APPLICATION_JSON);
         MvcResult result = mockMvc.perform(request).andExpect(status().isOk()).andReturn();
-        String expected = "{ \"companyName\": \"companyName1\"}";
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+        JSONAssert.assertEquals(organisationContext.jsonString(), result.getResponse().getContentAsString(), true);
     }
 
     @Test
     public void getOrganisationListTest() throws Exception {
-        List<Organisation> organisationList = getOrganisationList();
-        when(organisationService.getOrganisations()).thenReturn(organisationList);
+        when(organisationService.getOrganisations()).thenReturn(getOrganisationList());
         RequestBuilder request = MockMvcRequestBuilders.get("/organisations").accept(MediaType.APPLICATION_JSON);
         MvcResult result = mockMvc.perform(request)
                 .andExpect(status().isOk())
                 .andReturn();
-        String expected = "[ { \"companyName\": \"companyName1\"} ]";
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
-        //TODO: Map Site in builder class
-        //JSONAssert.assertEquals(expectedResponseList, result.getResponse().getContentAsString(), true);
+        JSONAssert.assertEquals(organisationListContext.jsonString(), result.getResponse().getContentAsString(), true);
     }
 
     @Test
@@ -62,17 +69,15 @@ public class OrganisationControllerTest extends BaseTest {
         when(organisationService.getSiteByID(anyString())).thenReturn(getSite());
         RequestBuilder request = MockMvcRequestBuilders.get("/organisation/sites/{id}", "siteId").accept(MediaType.APPLICATION_JSON);
         MvcResult result = mockMvc.perform(request).andExpect(status().isOk()).andReturn();
-        String expected = "{ \"siteId\": \"siteId\"}";
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+        JSONAssert.assertEquals(siteContext.jsonString(), result.getResponse().getContentAsString(), true);
     }
 
     @Test
     public void createOrganisationTest() throws Exception {
         when(organisationService.createOrganisation(any())).thenReturn(getOrganisation());
-        RequestBuilder request = MockMvcRequestBuilders.post("/organisation").content(expectedResponse).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
+        RequestBuilder request = MockMvcRequestBuilders.post("/organisation").content(organisationContext.jsonString()).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
         MvcResult result = mockMvc.perform(request).andExpect(status().isCreated()).andReturn();
-        String expected = "{ \"companyName\": \"companyName1\"}";
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+        JSONAssert.assertEquals(organisationContext.jsonString(), result.getResponse().getContentAsString(), true);
 
     }
 
@@ -80,10 +85,10 @@ public class OrganisationControllerTest extends BaseTest {
     public void addSiteTest() throws Exception {
         when(organisationService.getOrganisationById(anyString())).thenReturn(getOrganisation());
         when(organisationService.addSite(any())).thenReturn(getSite());
-        RequestBuilder request = MockMvcRequestBuilders.post("/organisation/{id}/site", "orgId").content(expectedSiteResponse)
+        RequestBuilder request = MockMvcRequestBuilders.post("/organisation/{id}/site", "orgId").content(siteContext.jsonString())
                 .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
         MvcResult result = mockMvc.perform(request).andExpect(status().isOk()).andReturn();
-        String expected = "Site(siteId=siteId, name=site1, floors=null, orgId=orgId)";
+        String expected = "Site(siteId=siteId, name=site1, floors=[Floor(floorId=floorId, name=floor1, totalSeat=totalSeat, siteId=siteId)], orgId=orgId)";
         assertEquals(expected, result.getResponse().getContentAsString());
     }
 
