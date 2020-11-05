3c2778f93ee67562608ef94ed8641f338b52d1cb
kanwalnainsingh <mail@kanwalnain.com>
2020-11-05 18:51:25 +0530

    delete extra files.

--- code_base/micro-services/organisation-service/src/main/java/github/opendesk/organisationservice/dao/SiteRepository.java
+++
@@ -1,8 +0,0 @@
-package github.opendesk.organisationservice.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SiteRepository extends CrudRepository<SiteDao, String> {
-}
