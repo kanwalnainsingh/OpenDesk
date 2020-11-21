63cbefa20720a46e22217879e1e01dc43bcc44a7
Pebrian Charliady <54937272+pebriancharliady@users.noreply.github.com>
2020-11-21 13:32:22 +0700

    Work In Progress page (#83)
    * Create WIP Page, still have some performance issues to fix
    
    * Creating breakpoints & clean up the code little bit
    
    * update

--- code_base/web-ui/src/router/Routes.js
+++ code_base/web-ui/src/router/Routes.js
@@ -1,14 +1,15 @@
 import React, { Suspense, lazy } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
+
 import { Spinner } from '../app/components/Spinner'
 import SetupBuilder from '../app/container/SetupBuilder/SetupBuilder'
 import Site from '../app/components/Site/Site'
 import AddSite from '../app/container/AddSite/AddSite'
 const HomeRouter = lazy(() => import("./HomeRouter"))
+const WIPRouter = lazy(() => import("./WIPRouter"));
 
 
 export const Routes = () => {
-  
   return (
     <Suspense fallback={<Spinner />}>
     <Switch>
@@ -20,6 +21,7 @@ export const Routes = () => {
 
       <Route path="/organization" component={AddSite} />
       <Route path="/tesOrganisation" component={SetupBuilder} />
+      <Route path="/wip" component={WIPRouter} />
     </Switch>
   </Suspense>
   );
