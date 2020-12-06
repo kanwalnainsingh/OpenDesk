9e54e9d3dda14bdb563ab5cc241792faeab711da
chimeziesolomon <chimezie.solomon.cs@gmail.com>
2020-12-06 10:43:54 +0100

    new commit

---
+++ my-app/src/Login/Container.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Login from './Login/Login';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+	root: {
+		background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+		border: 0,
+		borderRadius: 3,
+		boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+		color: 'white',
+		height: 48,
+		padding: '0 30px'
+	}
+});
+export default function FixedContainer() {
+	const classes = useStyles();
+	return (
+		<React.Fragment>
+			<CssBaseline />
+			<Container className={classes.root}>
+				<Login />
+				<Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+			</Container>
+		</React.Fragment>
+	);
+}
