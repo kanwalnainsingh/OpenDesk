4d0634e6cef8993981c8339c44b4196e653b7726
yagnaguru.r <yagnaguru.r@hobsons.com>
2020-12-16 14:03:02 +0530

    spring-security: fixed test case failiure

--- code_base/micro-services/authentication-service/src/test/java/github/opendesk/authenticationtest/SpringSecurityJwtApplicationTests.java
+++ code_base/micro-services/authentication-service/src/test/java/github/opendesk/authenticationtest/SpringSecurityJwtApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class SpringSecurityJwtApplicationTests {
 
 	@Test
