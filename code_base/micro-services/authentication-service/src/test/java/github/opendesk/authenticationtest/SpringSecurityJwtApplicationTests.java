4b3e10aa9933e6ef670809eac733409ff8aaae01
yagnaguru.r <yagnaguru.r@hobsons.com>
2020-12-16 14:35:54 +0530

    test fix

--- code_base/micro-services/authentication-service/src/test/java/github/opendesk/authenticationtest/SpringSecurityJwtApplicationTests.java
+++ code_base/micro-services/authentication-service/src/test/java/github/opendesk/authenticationtest/SpringSecurityJwtApplicationTests.java
@@ -3,9 +3,11 @@ package github.opendesk.authenticationtest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration
 public class SpringSecurityJwtApplicationTests {
 
 	@Test
