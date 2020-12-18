78019e65ee7fe58e78a2334409fc7e2d363eefc3
mayralgr <ml17ft@gmail.com>
2020-12-17 19:45:42 -0600

    Clean imports

--- code_base/micro-services/organisation-service/src/main/java/github/opendesk/organisationservice/config/OrganisationConfig.java
+++ code_base/micro-services/organisation-service/src/main/java/github/opendesk/organisationservice/config/OrganisationConfig.java
@@ -1,10 +1,8 @@
 package github.opendesk.organisationservice.config;
 
-import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.web.client.RestTemplate;
 
--- code_base/micro-services/organisation-service/src/main/java/github/opendesk/organisationservice/config/RabbitMQSender.java
+++ code_base/micro-services/organisation-service/src/main/java/github/opendesk/organisationservice/config/RabbitMQSender.java
@@ -3,10 +3,6 @@ package github.opendesk.organisationservice.config;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 
 public class RabbitMQSender {
     @Autowired
